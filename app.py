dcd5831ef08e9fbc3a1237f1ec09693a2fd4c645
Y4nnLS <yann_lucas04@hotmail.com>
2024-05-02 19:32:22 -0300

    correção de bugs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template,redirect, url_for
 import hashlib
 from hash import hash_usersenha, hash_sla
 import requests
@@ -9,26 +9,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro', methods=['POST', 'GET'])
+def cadastro():
+    if(request.method == 'POST'):
+        features = request.json
+        X = []
+        senha = str(features['senha'])
+        nome = str(features['nome'])
+        userHash = hash_usersenha(nome, senha)
+        passwordHash = hash_sla(nome)
+        X.append(userHash)
+        X.append(passwordHash)
+        X.append(features['nome_completo'])
+        X.append(features['cpf'])
+        X.append(features['telefone'])
+        return jsonify(X)
     return render_template('cadastro.html')
 
-@app.route('/cadastro', methods=['POST'])
-def cadastro():
-    features = request.json
-    X = []
-    senha = str(features['senha'])
-    nome = str(features['nome'])
-    userHash = hash_usersenha(nome, senha)
-    passwordHash = hash_sla(nome)
-    X.append(userHash)
-    X.append(passwordHash)
-    X.append(features['nome_completo'])
-    X.append(features['cpf'])
-    X.append(features['telefone'])
-    
-    
-    
-   
-    return jsonify(X)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+p.route('/')
+# def index():
+#     return render_template('cadastro.html')
